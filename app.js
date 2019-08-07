061b951224250daeaf49bc6d9a2d781e438f1bde
Jay arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2019-08-07 20:39:12 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// Init Nexmo
+const nexmo = new Nexmo({
+  apiKey: '9fafe451',
+  apiSecret: '4INR87C3j1Qugqon'
+}, { debug: true });
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Catch form submit
+app.post('/', (req, res) => {
+  // res.send(req.body);
+  // console.log(req.body);
+  const { number, text } = req.body;
+
+  nexmo.message.sendSms(
+    '639150616814', number, text, { type: 'unicode' },
+    (err, responseData) => {
+      if(err) {
+        console.log(err);
+      } else {
+        const { messages } = responseData;
+        const { ['message-id']: id, ['to']: number, ['error-text']: error  } = messages[0];
+        console.dir(responseData);
+        // Get data from response
+        const data = {
+          id,
+          number,
+          error
+        };
+
+        // Emit to the client
+        io.emit('smsStatus', data);
+      }
+    }
+  );
+});
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+});
